28b89ee9a6c339aa8751c9eb54731360e2ad27e2
octavivs <abaddonvii@gmail.com>
2015-05-24 16:31:58 -0500

    Actualización del script de regsitro.

--- view/js/signup.js
+++ view/js/signup.js
@@ -26,9 +26,11 @@ function initialize() {
 
 function signUp() {
     var state = "ok";
-    var required = "<span class='required'>Esta información es obligatoria.</span>";
-    var invalid = "<span class='required'>Información incorrecta</span>";
-    var different = "<span class='required'>Las contraseñas no coinciden.</span>";
+    var already = "Ya hay otra persona con esta dirección de correo electrónico.";
+    var required = "Esta información es obligatoria.";
+    var invalid = "Información incorrecta.";
+    var different = "Las contraseñas no coinciden.";
+    var minor = "The birthdate indicates that you're under 13 years of age.";
     var firstName = $("#FirstName").val();
     var lastName = $("#LastName").val();
     var userName = $("#UserName").val();
@@ -42,40 +44,40 @@ function signUp() {
     var birthYear = $("#BirthYear").val();
     clearMessages();
     if ( firstName === '') {
-        $( required ).insertAfter("#FirstName");
+        $( message(required) ).insertAfter("#FirstName");
     }
     if ( lastName === '') {
-        $( required ).insertAfter("#LastName");
+        $( message(required) ).insertAfter("#LastName");
     }
     if ( userName === '') {
-        $( required ).insertAfter("#UserName");
+        $( message(required) ).insertAfter("#UserName");
     }
     if ( password === '') {
-        $( required ).insertAfter("#Password");
+        $( message(required) ).insertAfter("#Password");
     }
     if ( rePassword !== password ) {
-        $( different ).insertAfter("#RePassword");
-        state = "The passwords don't match.";
+        $( message(different) ).insertAfter("#RePassword");
     } else if ( rePassword === '') {
-        $( required ).insertAfter("#RePassword");
+        $( message(required) ).insertAfter("#RePassword");
     }
     if ( address === '' ) {
-        $( required ).insertAfter("#Address");
+        $( message(required) ).insertAfter("#Address");
     }
     if ( city === '' ) {
-        $( required ).insertAfter("#City");
+        $( message(required) ).insertAfter("#City");
     }
     if ( telephone === '' ) {
-        $( required ).insertAfter("#Telephone");
+        $( message(required) ).insertAfter("#Telephone");
     } else if ( !isInteger(telephone) ) {
-        $( invalid ).insertAfter("#Telephone");
+        $( message(invalid) ).insertAfter("#Telephone");
     }
     if ( birthMonth === '' || birthDay === '' || birthYear === '' ) {
-        $( required ).insertAfter("#BirthYear");
+        $( message(required) ).insertAfter("#BirthYear");
     }
     if ( checkAge(birthYear, birthMonth, birthDay) < 13 ) {
-        $("<span id='required'>The birthdate indicates that you're under 13 years of age.</span>").insertAfter("#BirthYear");
+        $( message(minor) ).insertAfter("#BirthYear");
     }
+    /*
     $.post(
         "view/js/register.php",
         {first_name: firstName, last_name: lastName, username: userName, pass: password, address: address, city: city, telephone: telephone },
@@ -83,13 +85,15 @@ function signUp() {
             alert(data);
         }
     );
-    // Ya hay otra persona que tiene esta dirección de correo electrónico.
-    // Las contraseñas no coinciden.
-    // The birthdate indicates that you're under 13 years of age.
+    */
 }
 
 function isInteger(x) {
     return (typeof x === 'number') && (x % 1 === 0);
+}
+
+function message(text) {
+    return "<span class='required'>"+text+"</span>";
 }
 
 function clearMessages() {
