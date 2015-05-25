fb993bc6d361631a4d320d065dddbd0633acff57
dan68 <guti_dani2@hotmail.com>
2015-05-25 10:43:24 -0500

    se agrego la pagina de servicios

---
+++ view/servicios.tpl.php
@@ -0,0 +1,62 @@
+<div class="row">
+    <div class="large-4 columns" id="ejem">
+        <h4>Playeras</h4>
+        <img src="view/img/playera sublimadas.jpg" />
+    </div>
+    <div class="large-4 columns" id="ejem">
+        <h4>Mouse pack</h4>
+    </div>
+    <div class="large-4  columns" id="ejem">
+        <h4>Platos</h4>
+    </div>
+</div>
+
+<div class="row">
+    <div class="large-4 columns" id="ejem">
+        <h4>Rompecabezas </h4>
+    </div>
+    <div class="large-4 columns" id="ejem">
+        <h4>Almohadas</h4>
+    </div>
+    <div class="large-4  columns" id="ejem">
+        <h4>Tazas</h4>
+    </div>
+</div>
+
+<div class="row">
+    <div class="large-4 columns" id="ejem">
+        <h4>Fotobotones </h4>
+    </div>
+    <div class="large-4 columns" id="ejem">
+        <h4>Flyers digital</h4>
+    </div>
+    <div class="large-4  columns" id="ejem">
+        <h4>Flyers 1 tinta</h4>
+    </div>
+</div>
+
+<div class="row">
+    <div class="large-4 columns" id="ejem">
+        <h4>Tarjetas </h4>
+        <img src="view/img/JUEVES DE TARJETAS.jpg" />
+    </div>
+    <div class="large-4 columns" id="ejem">
+        <h4>Credencial 4x1</h4>
+    </div>
+    <div class="large-4  columns" id="ejem">
+        <h4>Clases 4x4</h4>
+    </div>
+</div>
+
+<div class="row">
+    <div class="large-4 columns" id="ejem">
+        <h4>serigrafia</h4>
+    </div>
+    <div class="large-4 columns" id="ejem">
+        <h4>lonas </h4>
+        <img src="view/img/LONA IMPRESA.jpg" />
+    </div>
+    <div class="large-4  columns" id="ejem">
+        <h4></h4>
+    </div>
+</div>
