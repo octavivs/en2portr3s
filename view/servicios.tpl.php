4ac3a739d8bca79607db60a02f9e063b936c8e3a
dan68 <guti_dani2@hotmail.com>
2015-05-25 11:50:42 -0500

    se modifique imagenes de servicios

--- view/servicios.tpl.php
+++ view/servicios.tpl.php
@@ -1,62 +1,73 @@
 <div class="row">
     <div class="large-4 columns" id="ejem">
         <h4>Playeras</h4>
-        <img src="view/img/playera sublimadas.jpg" />
+        <img src="view/img/Captura-de-pantalla-(571).png" />
     </div>
     <div class="large-4 columns" id="ejem">
         <h4>Mouse pack</h4>
+       <img src="view/img/Captura-de-pantalla5711.png" />
     </div>
     <div class="large-4  columns" id="ejem">
         <h4>Platos</h4>
+        <img src="view/img/Captura-de-pantalla-(572).png" />
     </div>
 </div>
 
 <div class="row">
     <div class="large-4 columns" id="ejem">
         <h4>Rompecabezas </h4>
+          <img src="view/img/Captura-de-pantalla-5722.png" />
     </div>
     <div class="large-4 columns" id="ejem">
         <h4>Almohadas</h4>
+          <img src="view/img/Captura-de-pantalla-(573).png" />
     </div>
     <div class="large-4  columns" id="ejem">
         <h4>Tazas</h4>
+          <img src="view/img/Captura-de-pantalla-5733.png" />
     </div>
 </div>
 
 <div class="row">
     <div class="large-4 columns" id="ejem">
         <h4>Fotobotones </h4>
+          <img src="view/img/Captura-de-pantalla-(574).png" />
     </div>
     <div class="large-4 columns" id="ejem">
         <h4>Flyers digital</h4>
+          <img src="view/img/Captura-de-pantalla-(575).png" />
     </div>
     <div class="large-4  columns" id="ejem">
         <h4>Flyers 1 tinta</h4>
+          <img src="view/img/Captura-de-pantalla-(575)5.png" />
     </div>
 </div>
 
 <div class="row">
     <div class="large-4 columns" id="ejem">
         <h4>Tarjetas </h4>
-        <img src="view/img/JUEVES DE TARJETAS.jpg" />
+        <img src="view/img/Captura-de-pantalla-(576).png" />
     </div>
     <div class="large-4 columns" id="ejem">
         <h4>Credencial 4x1</h4>
+          <img src="view/img/Captura-de-pantalla-(577).png" />
     </div>
     <div class="large-4  columns" id="ejem">
-        <h4>Clases 4x4</h4>
+        <h4>Credenciales 4x4</h4>
+           <img src="view/img/Captura-de-pantalla-(577).png" />
     </div>
 </div>
 
 <div class="row">
     <div class="large-4 columns" id="ejem">
         <h4>serigrafia</h4>
+          <img src="view/img/Captura-de-pantalla-(578).png" />
     </div>
     <div class="large-4 columns" id="ejem">
         <h4>lonas </h4>
         <img src="view/img/LONA IMPRESA.jpg" />
     </div>
     <div class="large-4  columns" id="ejem">
-        <h4></h4>
+        <h4> </h4>
     </div>
 </div>
