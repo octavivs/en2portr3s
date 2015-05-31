20b64abd5a4c1221f210203c6d4b866c4207fdd3
octavivs <abaddonvii@gmail.com>
2015-05-31 17:44:23 -0500

    Ronombrado de los archivos que se utilizan en la página de servicios y la actualización de los enlaces que aparecen en esa página.

--- view/servicios.tpl.php
+++ view/servicios.tpl.php
@@ -2,60 +2,60 @@
     <div class="row">
         <div class="large-4 columns">
             <input type="button" class="button expand" value="Playeras"/>
-            <img src="view/img/Captura-de-pantalla-(571).png" class="hidden-for-small" />
+            <img src="view/img/playeras.png" class="hidden-for-small" />
         </div>
         <div class="large-4 columns">
             <input type="button" class="button expand" value="Mouse pack"/>
-            <img src="view/img/Captura-de-pantalla5711.png" class="hidden-for-small"/>
+            <img src="view/img/mouse_pack.png" class="hidden-for-small"/>
         </div>
         <div class="large-4 columns">
             <input type="button" class="button expand" value="Platos"/>
-            <img src="view/img/Captura-de-pantalla-(572).png" class="hidden-for-small" />
+            <img src="view/img/platos.png" class="hidden-for-small" />
         </div>
     </div>
 
     <div class="row">
         <div class="large-4 columns">
             <input type="button" class="button expand" value="Rompecabezas"/>
-            <img src="view/img/Captura-de-pantalla-5722.png" class="hidden-for-small" />
+            <img src="view/img/rompecabezas.png" class="hidden-for-small" />
         </div>
         <div class="large-4 columns">
             <input type="button" class="button expand" value="Almohadas" />
-            <img src="view/img/Captura-de-pantalla-(573).png" class="hidden-for-small" />
+            <img src="view/img/almohadas.png" class="hidden-for-small" />
         </div>
         <div class="large-4 columns">
             <input type="button" class="button expand" value="Tazas"/>
-            <img src="view/img/Captura-de-pantalla-5733.png" class="hidden-for-small" />
+            <img src="view/img/tazas.png" class="hidden-for-small" />
         </div>
     </div>
 
     <div class="row">
         <div class="large-4 columns">
             <input type="button" class="button expand" value="Fotobotones"/>
-            <img src="view/img/Captura-de-pantalla-(574).png" class="hidden-for-small" />
+            <img src="view/img/fotobotones.png" class="hidden-for-small" />
         </div>
         <div class="large-4 columns">
             <input type="button" class="button expand" value="Flyers digital"/>
-            <img src="view/img/Captura-de-pantalla-(575).png" class="hidden-for-small" />
+            <img src="view/img/flyers_digital.png" class="hidden-for-small" />
         </div>
         <div class="large-4 columns">
             <input type="button" class="button expand" value="Flyers 1 tinta"/>
-            <img src="view/img/Captura-de-pantalla-(575)5.png" class="hidden-for-small" />
+            <img src="view/img/flyers_1_tinta.png" class="hidden-for-small" />
         </div>
     </div>
 
     <div class="row">
         <div class="large-4 columns">
             <input type="button" class="button expand" value="Tarjetas"/>
-            <img src="view/img/Captura-de-pantalla-(576).png" class="hidden-for-small" />
+            <img src="view/img/tarjetas.png" class="hidden-for-small" />
         </div>
         <div class="large-4 columns">
             <input type="button" class="button expand" value="Credencial 4x1"/>
-            <img src="view/img/Captura-de-pantalla-(577).png" class="hidden-for-small" />
+            <img src="view/img/credencial_4x1.png" class="hidden-for-small" />
         </div>
         <div class="large-4 columns">
             <input type="button" class="button expand" value="Credenciales 4x4"/>
-            <img src="view/img/Captura-de-pantalla-(577).png" class="hidden-for-small" />
+            <img src="view/img/credenciales_4x4.png" class="hidden-for-small" />
         </div>
     </div>
 
