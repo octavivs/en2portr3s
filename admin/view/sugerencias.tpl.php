6d7a31f594c2da08d7d11d6f95b5bd8cc40bae24
Octavio <abaddonvii@gmail.com>
2015-07-13 13:23:13 -0500

    * Se quitaron divs innecesarios y una etiqueta </tr> repetida en la vista Sugerencias.

--- admin/view/sugerencias.tpl.php
+++ admin/view/sugerencias.tpl.php
@@ -5,25 +5,21 @@ use en2portr3s\model\Suggestion;
 $suggestion = new Suggestion();
 $datos = $suggestion->get();
 ?>
-<div class="row">
-    <div class="large-12 columns">
-        <table class="responsive">
-            <tr>
-                <th>ID</th>
-                <th>Mensaje</th>
-                <th>Fecha</th>
-                <th>Eliminar</th>
-            </tr>
-            <?php
-            foreach ($datos as $dato) {
-                echo "<tr>" . PHP_EOL;
-                echo "<td ><input type='text' id='id' name='id' value='" . $dato['id'] . "'  /></td>" . PHP_EOL;
-                echo "<td >" . $dato['content'] . "</td>" . PHP_EOL;
-                echo "<td  ><input type='text' name='since' value='" . $dato['since'] . "'  /></td>" . PHP_EOL;
-                echo "<td ><input type='button' class='button tiny' id='eliminar' value='Eliminar' /></td></tr>" . PHP_EOL;
-                echo "</tr>" . PHP_EOL;
-            }
-            ?>
-        </table>
-    </div>
-</div>
+<table class="responsive">
+    <tr>
+        <th>ID</th>
+        <th>Mensaje</th>
+        <th>Fecha</th>
+        <th>Eliminar</th>
+    </tr>
+    <?php
+    foreach ($datos as $dato) {
+        echo "<tr>" . PHP_EOL;
+        echo "<td ><input type='text' id='id' name='id' value='" . $dato['id'] . "'  /></td>" . PHP_EOL;
+        echo "<td >" . $dato['content'] . "</td>" . PHP_EOL;
+        echo "<td  ><input type='text' name='since' value='" . $dato['since'] . "'  /></td>" . PHP_EOL;
+        echo "<td ><input type='button' class='button tiny' id='eliminar' value='Eliminar' /></td>" . PHP_EOL;
+        echo "</tr>" . PHP_EOL;
+    }
+    ?>
+</table>
