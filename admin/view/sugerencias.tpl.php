91353c139524b33147c123cd65138c5e0ca09755
dan68 <guti_dani2@hotmail.com>
2015-07-13 14:33:37 -0500

    ya se puede eliminar correctamente las sugerencias

--- admin/view/sugerencias.tpl.php
+++ admin/view/sugerencias.tpl.php
@@ -13,13 +13,18 @@ $datos = $suggestion->get();
         <th>Eliminar</th>
     </tr>
     <?php
+    
+    
+   
+    
     foreach ($datos as $dato) {
         echo "<tr>" . PHP_EOL;
         echo "<td ><input type='text' id='id' name='id' value='" . $dato['id'] . "'  /></td>" . PHP_EOL;
         echo "<td >" . $dato['content'] . "</td>" . PHP_EOL;
         echo "<td  ><input type='text' name='since' value='" . $dato['since'] . "'  /></td>" . PHP_EOL;
-        echo "<td ><input type='button' class='button tiny' id='eliminar' value='Eliminar' /></td>" . PHP_EOL;
+        echo "<td ><input type='button' class='button tiny' data-id='" .$dato['id']."' id='eliminar' value='Eliminar' /></td>" . PHP_EOL;
         echo "</tr>" . PHP_EOL;
+         
     }
     ?>
 </table>
