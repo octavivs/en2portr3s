446a37299feb06ca6d4cd0b03ab9d79443dd1a3f
Octavio <abaddonvii@gmail.com>
2015-07-20 14:40:16 -0500

    Se mejoró la página de del gestor de contenido.

--- admin/view/contenido.tpl.php
+++ admin/view/contenido.tpl.php
@@ -1,41 +1,43 @@
 <?php
+
 use en2portr3s\model\Page;
 
 $page = new Page();
 $list = $page->get();
-$is_active = true;
+$is_active;
 ?>
 <div>
     <ul class="tabs" data-tab>
         <?php
+        $is_active = true;
         foreach ($list as $item) {
             $id = $item['label'];
-            $name = ucfirst($id);
             if ($is_active) {
-                echo "<li class = 'tab-title active'><a href = '#$id'>$name</a></li>";
+                echo "<li class='tab-title active'><a href='#$id'>$id</a></li>";
                 $is_active = false;
             } else {
-                echo "<li class = 'tab-title'><a href = '#$id'>$name</a></li>";
+                echo "<li class='tab-title'><a href='#$id'>$id</a></li>";
             }
         }
         ?>
     </ul>
 
     <div class="tabs-content">
-        <div class="content active" id="inicio">
-            <p>This is the first panel of the basic tab example. You can place all sorts of content here including a grid.</p>
-        </div>
-        <div class="content" id="servicios">
-            <p>This is the second panel of the basic tab example. This is the second panel of the basic tab example.</p>
-        </div>
-        <div class="content" id="conocenos">
-            <p>This is the third panel of the basic tab example. This is the third panel of the basic tab example.</p>
-        </div>
-        <div class="content" id="contacto">
-            <p>This is the fourth panel of the basic tab example. This is the fourth panel of the basic tab example.</p>
-        </div>
-        <div class="content" id="registro">
-            <p>This is the fourth panel of the basic tab example. This is the fourth panel of the basic tab example.</p>
-        </div>
+        <?php
+        $is_active = true;
+        foreach ($list as $item) {
+            $id = $item['label'];
+            if ($is_active) {
+                echo "<div class='content active' id='$id'>";
+                echo "<p>This is the $id panel of the basic tab example.</p>";
+                echo "</div>";
+                $is_active = false;
+            } else {
+                echo "<div class='content' id='$id'>";
+                echo "<p>This is the $id panel of the basic tab example.</p>";
+                echo "</div>";
+            }
+        }
+        ?>
     </div>
 </div>
