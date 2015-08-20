d999a3995cd453396b22e7f09cc9e5e1688af791
Backtun <kevindominguezriquelme@192.168.1.82>
2015-08-20 13:08:27 -0500

    Cambios en la pagina de servicios, y en el archivo de app.css

--- view/servicios.tpl.php
+++ view/servicios.tpl.php
@@ -1,32 +1,40 @@
-<div id="services">
-    <div class="row">
-        <div class="medium-6 large-4 columns"  style="padding-left: 0px;padding-right: 0px;">
+<style type="text/css">
+div.large-12{
+  padding: 0;
+}
 
-            <a href="lonaimpresa"> <img src="{tpl_content_13_url}" alt="{tpl_content_13_alt}" class="" /></a> 
-        </div>
-        <div class="medium-6 large-4 columns"  style="padding-left: 0px;padding-right: 0px;">
+#page-content {
+    margin-bottom:0px;
+}
 
-            <a href="vinil"> <img src="{tpl_content_15_url}" alt="{tpl_content_15_alt}" class="" /> </a>
-        </div>
-        <div class="medium-6 large-4 columns"  style="padding-left: 0px;padding-right: 0px;">
+h1{
+  color: #f7f7f7;
+  text-shadow: 0px 0px 10px rgba(0, 0, 0, 0.8); 
+}
 
-            <a href="sublimacion"> <img src="{tpl_content_17_url}" alt="{tpl_content_17_alt}" class="" /></a>  
-        </div>
+.row{
+  max-width: 100%
+}
+</style>
+
+<section id="secciones" class="fullScreen">
+    
+    <div class="half halfRooms">
+        <img src="view/img/LONA SERVICIOS.png">
     </div>
-
-    <div class="row">
-        <div class="medium-6 large-4 columns" style="padding-left: 0px;padding-right: 0px;">
-            <a href="serigrafia">  <img src="{tpl_content_19_url}" alt="{tpl_content_19_alt}" class="" /></a>  
-
-        </div>
-        <div class="medium-6 large-4 columns"  style="padding-left: 0px;padding-right: 0px;">
-            <a href="rotulacion"> <img src="{tpl_content_21_url}" alt="{tpl_content_21_alt}" class="" /></a>
-
-        </div>
-        <div class="medium-6 large-4 columns"  style="padding-left: 0px;padding-right: 0px;">
-            <a href="vinilcorte"> <img src="{tpl_content_23_url}" alt="{tpl_content_23_alt}" class="" /></a>
-
-        </div>
+    <div class="half halfRooms">
+        <img src="view/img/VINIL SERVICIOS.png">
     </div>
-
-</div>
+    <div class="half halfRooms">
+        <img src="view/img/SUBLIMACION SERVICIOS.png">
+    </div>
+    <div class="half halfRooms">
+        <img src="view/img/SERIGRAFIA SERVICIOS.png">
+    </div>
+    <div class="half halfRooms">
+        <img src="view/img/ROTULACION SERVICIOS.png">
+    </div>
+    <div class="half halfRooms">
+        <img src="view/img/RECORTE SERVICIOS.png">
+    </div>
+</section>
