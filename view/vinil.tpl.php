8b48692ae6fbd47109699e24d064f2c8262d776f
dan68 <guti_dani2@hotmail.com>
2015-08-31 11:00:04 -0500

    cambio de color al chat y se pusieron los rows para poner las imagenes de servicios

--- view/vinil.tpl.php
+++ view/vinil.tpl.php
@@ -1,7 +1,8 @@
 <?php
 ?>
-<h3>Email de contacto</h3>
-<form action="respuesta/send" method="post" enctype="multipart/form-data">
-    <label for="contenido">Mensaje del wds:</label>
-    
-</form>
+ <div class="row">
+            
+    </div>
+<div class="row">
+       
+    </div>
