0cc7c1bc385a069ce57514afa9be3d19eb63a9bc
dan68 <guti_dani2@hotmail.com>
2015-09-10 10:54:36 -0500

    se esta trabajando con rotulacion y serigrafia

--- view/serigrafia.tpl.php
+++ view/serigrafia.tpl.php
@@ -1,9 +1,22 @@
 <?php
 ?>
-  <div class="row">
-            
-    </div>
-<div class="row">
-       
-    </div>
+<div id="seri" class="row">
+    <div class="medium-5 medium-5 columns" >
+        <img  src="view/img/SERI.png">  
+    </div>     
 
+    <div  class="medium-5 medium-5 columns" id="ser">
+        <p></p>
+        <h5></h5>
+        <p></p>
+        <h5></h5>
+
+    </div>
+    <div class="medium-1 medium-1 columns" id="graf">
+        <img  src="view/img/FLECHA.png">  
+    </div>
+    
+</div>
+<div   class="row" id="gra">
+ <img  src="view/img/pulpo.png"> 
+</div>
--- view/sublimacion.tpl.php
+++ view/sublimacion.tpl.php
@@ -1,8 +1,22 @@
 <?php
 ?>
-  <div class="row">
-            
+<div id="subli" class="row">
+    <div class="medium-5 medium-5 columns" >
+        <img  src="view/img/.png">  
+    </div>     
+
+    <div  class="medium-5 medium-5 columns" id="sub">
+        <p></p>
+        <h5></h5>
+        <p></p>
+        <h5></h5>
+
     </div>
-<div class="row">
-       
+    <div class="medium-1 medium-1 columns" id="su">
+        <img  src="view/img/FLECHA.png">  
     </div>
+    
+</div>
+<div   class="row" id="subl">
+ <img  src="view/img/PLANCHA.png"> 
+</div>
